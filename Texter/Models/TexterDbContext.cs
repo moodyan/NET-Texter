73aaf20aa5edb753d1fae7193485e11e8ffaac5f
Alyssa Moody <alyssanicholemoody@gmail.com>
2017-08-24 10:15:50 -0700

    Create inital files and folders. Add RestSharp to make HTTP requests. Add GetMessages() and Send() methods to message class.

---
+++ Texter/Models/TexterDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Texter.Models
+{
+    public class TexterDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public TexterDbContext() { }
+        public DbSet<Message> Messages { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Texter;integrated security=True");
+        }
+        public TexterDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
